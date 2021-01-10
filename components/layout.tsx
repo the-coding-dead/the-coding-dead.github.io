f64fa6e26625d8df91d942358cf95c45503ccae2
nas <nas.silver.bullet@gmail.com>
2021-01-11 01:39:55 +0900

    fix

--- components/layout.tsx
+++ components/layout.tsx
@@ -15,13 +15,11 @@ const Layout = ({
       <link rel="icon" href="/favicon.ico" />
       <meta
         name="description"
-        content="Learn how to build a personal website using Next.js"
+        content={configs.siteTitle}
       />
       <meta
         property="og:image"
-        content={`https://og-image.now.sh/${encodeURI(
-          configs.siteTitle,
-        )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        content="/images/og-image.svg"
       />
       <meta name="og:title" content={configs.siteTitle} />
       <meta name="twitter:card" content="summary_large_image" />
