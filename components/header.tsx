1e35e98f84f259a03e851c973fbcb1ba45f6d701
nas <nas.silver.bullet@gmail.com>
2021-01-12 01:34:43 +0900

    fix header footer

--- components/header.tsx
+++ components/header.tsx
@@ -1,8 +1,8 @@
 import Link from 'next/link';
 
 const Header = () => (
-  <div className="bg-gray-800 flex justify-start p-3">
-    <div className="text-3xl hover:opacity-75 ">
+  <div className="bg-gray-800 h-14 p-3">
+    <div className="text-3xl hover:opacity-75">
       <Link href="/"> The Coding Dead </Link>
     </div>
   </div>
