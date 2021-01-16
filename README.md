6efd84b788fd065defff667f7b9d59784c607867
nas <nas.silver.bullet@gmail.com>
2021-01-17 01:33:56 +0900

    fix

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 [![Actions Status](https://github.com/the-coding-dead/the-coding-dead.github.io/workflows/deploy/badge.svg)](https://github.com/the-coding-dead/the-coding-dead.github.io/actions)
 
 # The Coding Dead
+
+コードを窒息するほど読んで、スーパーエンジニアになった気になる
--- lib/configs.ts
+++ lib/configs.ts
@@ -2,7 +2,7 @@ const configs = {
   siteTitle: 'The Coding Dead',
   ogImage:
     'https://raw.githubusercontent.com/the-coding-dead/the-coding-dead.github.io/main/public/images/og-image.png',
-  description: 'コードについてだけを書いていく。なぜなら好きだから。',
+  description: 'コードを窒息するほど読んで、スーパーエンジニアになった気になる',
 };
 
 export default configs;
