53a39f7e3adb1cc1eb52e9664bf7afafa5af10cf
nas <nas.silver.bullet@gmail.com>
2021-01-17 01:00:37 +0900

    fix code block

--- lib/posts.ts
+++ lib/posts.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import matter from 'gray-matter';
 import remark from 'remark';
 import html from 'remark-html';
-import highlight from 'remark-highlight.js';
+import prism from 'remark-prism';
 
 const postsDirectory = path.join(process.cwd(), 'posts');
 const codesDirectory = path.join(process.cwd(), 'public/codes');
@@ -84,7 +84,7 @@ export const getPostData = async (id: string): Promise<PostData> => {
   // Use remark to convert markdown into HTML string
   const processedContent = await remark()
     .use(html)
-    .use(highlight)
+    .use(prism)
     .process(matterResult.content);
   const contentHtml = processedContent.toString();
 
