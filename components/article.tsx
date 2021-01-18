5b49e8bb20d66f9d5104d830863dd56ec5dfd950
nas <nas.silver.bullet@gmail.com>
2021-01-19 01:55:01 +0900

    fix article

--- components/article.tsx
+++ components/article.tsx
@@ -20,20 +20,22 @@ const Article = ({ postData }: { postData: PostData }) => (
   <article className="relative container mt-5 p-3 bg-gray-600 rounded">
     <Pin />
     <h1 className="text-3xl mb-1">{postData.title}</h1>
-    {postData.serialization !== null && postData.serialization.length !== 0 && (
-      <div className="text-sm">
-        <p>Related articles</p>
-        <Serialization thisId={postData.title} ids={postData.serialization} />
-      </div>
+    {postData.serialization !== undefined
+      && postData.serialization.length !== 0 && (
+        <div className="text-sm">
+          <p>Related articles</p>
+          <Serialization thisId={postData.title} ids={postData.serialization} />
+        </div>
     )}
     <Date dateString={postData.date} />
     <Code language={postData.language} code={postData.code} />
     <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-    {postData.serialization !== null && postData.serialization.length !== 0 && (
-      <div>
-        <h1 className="text-3xl mb-1">Related articles</h1>
-        <Serialization thisId={postData.title} ids={postData.serialization} />
-      </div>
+    {postData.serialization !== undefined
+      && postData.serialization.length !== 0 && (
+        <div>
+          <h1 className="text-3xl mb-1">Related articles</h1>
+          <Serialization thisId={postData.title} ids={postData.serialization} />
+        </div>
     )}
   </article>
 );
